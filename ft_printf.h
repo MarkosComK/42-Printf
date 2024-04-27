1c00224f925cf79c0982f054bbdbea7c2b54b2b4
MarkosComK <markoscomks@gmail.com>
2024-04-27 22:26:22 +0100

    Added basic struct for format

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 15:16:13 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/27 19:13:06 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/27 22:23:53 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
+typedef	struct
+{
+	int	minus;
+	int	plus;
+	int	space;
+	int	hash;
+	int	zero;
+	int	width;
+	int	prec;
+}
 
 int	ft_printf(char *format, ...);
 
