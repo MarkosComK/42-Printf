8b4e167cd2b93cf057dbe821f093e949d794dde8
MarkosComK <markoscomks@gmail.com>
2024-04-28 22:08:57 +0100

    Added func to print ptr adresses

--- print_format.c
+++ print_format.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 13:27:59 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/28 15:47:19 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/28 21:35:53 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	print_format(t_flags *flags, char *str, int *pos, va_list args)
 		count += putcharf(flags, va_arg(args, int));
 	if (str[*pos] == 's')
 		count += putstrf(flags, va_arg(args, char *));
+	if (str[*pos] == 'p')
+		count += print_ptr(flags, va_arg(args, size_t));
 	(*pos)++;
 	return (count);
 }
