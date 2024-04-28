d0941f5e84f8045785e9d339f7c42d1799751b5e
markoscomks <markoscomks@gmail.com>
2024-04-28 12:29:09 +0100

    Added ft putchar to code

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,32 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 15:49:01 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/27 19:21:42 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/28 12:23:49 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(char *format, ...)
+int	ft_printf(char *str, ...)
 {
 	va_list	args;
+	t_flags	flags;
 	int		i;
+	int		count;
 
 	i = 0;
-	va_start(args, format);
-	ft_putstr_fd(format, 1);
+	count = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		ft_bzero(&flags, sizeof(t_flags));
+		if (str[i] == '%')
+		{
+			ft_putchar('%');
+		}
+		count += ft_putchar(str[i]);
+		i++;
+	}
 	va_end(args);
-	return (0);
+	return (count);
 }
