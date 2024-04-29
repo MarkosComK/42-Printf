065111f1184d9bf2c3a3ffc34577298933f14a69
markoscomks <markoscomks@gmail.com>
2024-04-29 13:23:32 +0100

    New funcs to print ptr

---
+++ print_ptr_hex.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_ptr_hex.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:58:57 by marsoare          #+#    #+#             */
+/*   Updated: 2024/04/29 13:05:48 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	print_ptr_hex(int number, char x)
+{
+	int	printed;
+
+	printed = 0;
+	if (number < 16)
+	{
+		if (x == 'x')
+			return (printed += ft_putchar("0123456789abcdef"[number]));
+		else if (x == 'X')
+			return (printed += ft_putchar("0123456789ABCDEF"[number]));
+		return (printed);
+	}
+	printed += print_ptr_hex(number / 16, x);
+	printed += print_ptr_hex(number % 16, x);
+	return (printed);
+}
+
