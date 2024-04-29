ec8cfe1668ca3de335fc7c93ae3e010cdae62dde
markoscomks <markoscomks@gmail.com>
2024-04-29 14:41:07 +0100

    started decimal with flags

---
+++ print_decimal.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_decimal.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 14:30:55 by marsoare          #+#    #+#             */
+/*   Updated: 2024/04/29 14:40:54 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	print_decimal(t_flags *flags, int number)
+{
+	char	prefix;
+	char	sign;
+
+	prefix = ' ';
+	sign = '-';
+}
