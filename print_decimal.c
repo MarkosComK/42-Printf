b958aa38b6fa4dbcacb91d7365cde571ed36b148
markoscomks <markoscomks@gmail.com>
2024-05-04 18:53:45 +0100

    Prec + width working well

--- print_decimal.c
+++ print_decimal.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:30:55 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/01 14:05:31 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/05/04 16:01:35 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,7 @@ int	print_decimal(t_flags *flags, long number)
 	if (flags -> align_left)
 		return (print_decimal_left(number, flags->width,
 				flags->precision, sign));
+	if (flags -> width > 0 && flags -> precision > 0)
+		return (print_dec_wp(number, flags->width, flags->precision - 1, sign));
 	return (print_unsigned_dec(number));
 }
