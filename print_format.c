08771afc7e4b91d226da9075e41072ec48040050
markoscomks <markoscomks@gmail.com>
2024-05-04 21:40:15 +0100

    started print_hex func

--- print_format.c
+++ print_format.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 13:27:59 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/04 20:52:22 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/05/04 21:39:57 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int	print_format(t_flags *flags, char *str, int *pos, va_list args)
 		count += print_decimal(flags, va_arg(args, int));
 	if (str[*pos] == 'u')
 		count += print_decimal(flags, va_arg(args, unsigned int));
+	if (str[*pos] == 'x' || str[*pos] == 'X')
+		count += print_hexa(flags, va_arg(args, unsigned int), char[*pos]);
 	(*pos)++;
 	return (count);
 }
