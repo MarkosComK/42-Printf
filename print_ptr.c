cf88f7ae8a762a3bd82baee0a036c9f849e56aee
MarkosComK <markoscomks@gmail.com>
2024-05-09 11:08:51 +0100

    solved issue with flagh width ant null pointer

--- print_ptr.c
+++ print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 21:42:12 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/09 01:01:08 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/05/09 11:05:34 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	print_ptr(t_flags *flags, size_t number)
 {
 	int	string_len;
 
+	string_len = 0;
 	if (number == 0)
 	{
 		string_len = 5;
@@ -28,6 +29,10 @@ int	print_ptr(t_flags *flags, size_t number)
 				string_len += ft_putchar(' ');
 			return (string_len);
 		}
+		while (flags -> width > string_len)
+			string_len += ft_putchar(' ');
+		ft_putstr("(nil)");
+		return (string_len);
 	}
 	if (flags -> width > 0)
 		return (print_ptr_spaces(flags -> width, flags -> align_left, number));
