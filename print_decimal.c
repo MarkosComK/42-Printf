f72c435d097f2ed8530bf16bcce49c4c05582f9e
MarkosComK <markoscomks@gmail.com>
2024-05-09 02:39:43 +0100

    prefix flag in progress

--- print_decimal.c
+++ print_decimal.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:30:55 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/08 00:55:54 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/05/09 02:36:44 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,23 @@
 int	print_decimal(t_flags *flags, long number)
 {
 	char	sign;
+	char	pref;
 
 	sign = '-';
+	pref = ' ';
 	if (flags -> space)
 		sign = ' ';
 	if (flags -> plus)
 		sign = '+';
+	if (flags -> zero)
+		pref = '0';
 	if (flags -> align_left)
 		return (print_decimal_left(number, flags->width,
 				flags->precision - 1, sign));
 	if (flags -> width > 0 && flags -> precision > 0)
 		return (print_dec_wp(number, flags->width, flags->precision - 1, sign));
 	if (flags -> width)
-		return (print_dec_wp(number, flags->width, flags->precision - 1, sign));
+		return (print_dec_wz(number, flags->width, flags->precision - 1));
 	if (flags -> precision)
 		return (print_dec_wp(number, flags->width, flags->precision - 1, sign));
 	if (number < 0)
